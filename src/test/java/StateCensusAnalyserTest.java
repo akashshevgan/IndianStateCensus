894970f353ed2e904e0c24246062e1f9520951ca
akash <akash@MSI>
2021-05-23 18:07:52 +0530

    Added : TC 1.2 csv file incorrect return custom Exception

--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 
 public class StateCensusAnalyserTest {
     public static final String STATECENSUS_CSVFILE = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensusData.csv";
+    public static final String WRONG_FILE_PATH = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensu.csv";
 
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
@@ -17,5 +17,14 @@ public class StateCensusAnalyserTest {
             e.printStackTrace();
         }
     }
-}
 
+    @Test
+    public void GivenTheStateCensusCsvFile_IfDoesntExist_ShouldThrowCensusAnalyserException() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            int count = stateCensusAnalyser.loadCensusData(WRONG_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assertions.assertEquals("no such file exists.", e.getMessage());
+        }
+    }
+}
