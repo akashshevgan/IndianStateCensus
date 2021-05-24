265b5fc742f7b3a819bdecfc65c602d58a8f6964
akash <akash@MSI>
2021-05-24 22:23:33 +0530

    checking delimiter is improper

--- src/main/java/StateCensusAnalyser.java
+++ src/main/java/StateCensusAnalyser.java
@@ -14,8 +14,7 @@ public class StateCensusAnalyser {
     public int loadCensusData(String path) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(path));
-            CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(CSVStateCensus.class)
-                    .withIgnoreLeadingWhiteSpace(true).build();
+            CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(CSVStateCensus.class).withIgnoreLeadingWhiteSpace(true).build();
             Iterator<CSVStateCensus> csvUserIterator = csvToBean.iterator();
             while (csvUserIterator.hasNext()) {
                 count++;
@@ -23,7 +22,7 @@ public class StateCensusAnalyser {
             }
         } catch (RuntimeException exception) {
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.WRONG_FILE,
-                    "delimiter or header is improper");
+                    "delimiter is improper");
         } catch (NoSuchFileException exception) {
             throw new CensusAnalyserException(CensusAnalyserException.exceptionType.FILE_NOT_FOUND,
                     "file is not found");
--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -6,6 +6,7 @@ public class StateCensusAnalyserTest {
     public static final String STATECENSUS_CSVFILE = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensusData.csv";
     public static final String WRONG_FILE_PATH = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensu.csv";
     public static final String WRONG_FILE_TYPE = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensusData.txt";
+    public static final String WRONG_DELIMITER = "D:\\IndianStateCensus\\src\\test\\resources\\IndiaStateCensusData.csv";
 
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
@@ -36,6 +37,16 @@ public class StateCensusAnalyserTest {
             censusAnalyser.loadCensusData(WRONG_FILE_TYPE);
         } catch (CensusAnalyserException exception) {
             Assertions.assertEquals("file is not found", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void givenDelimiterErrorStateCsvFile_CheckPresentOrNot_ShouldReturnCustomException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            censusAnalyser.loadCensusData(WRONG_DELIMITER);
+        } catch (CensusAnalyserException exception) {
+            Assertions.assertEquals("delimiter is improper", exception.getMessage());
         }
     }
 }
